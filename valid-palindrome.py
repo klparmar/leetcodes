c58c9d150dee2bca983ded9f5c2865f345ffcd1b
KL Parmar <58920075+klparmar@users.noreply.github.com>
2022-11-22 03:13:04 -0500

    valid palindrome
    N/A

---
+++ valid-palindrome.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        x = len(s) - 1
+        while i < x:
+            while i < x and not s[i].isalpha():
+                i +=1
+            while i < x and not s[x].isalpha():
+                x -=1   
+            if(s[i].lower() != s[x].lower()):
+                return False
+                
+            i += 1
+            x -= 1
+        return True
+
+#       string = "".join([i for i in s if i.isalpha()])
+#       x = len(string) - 1
+#       i = 0
+#         for i in string:
+#             if(string.index(i) == x and len(s) == 1):
+#                 return True
+            
+#             if(i.lower() != string[x].lower()):
+#                 return False
+            
+#             x -=1
+#             print(i, string[x])
