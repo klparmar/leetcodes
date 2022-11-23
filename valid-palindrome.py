d12873ac13ccb15dcd8db5aba19810fe052faab4
KL Parmar <58920075+klparmar@users.noreply.github.com>
2022-11-23 00:04:01 -0500

    125: valid-palindrome.py
    Runtime: 204 ms, faster than 5.96% of Python3 online submissions for Valid Palindrome.
    Memory Usage: 14.9 MB, less than 32.22% of Python3 online submissions for Valid Palindrome.

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -3,16 +3,17 @@ class Solution:
         i = 0
         x = len(s) - 1
         while i < x:
-            while i < x and not s[i].isalpha():
+            while i < x and not s[i].isalnum():
                 i +=1
-            while i < x and not s[x].isalpha():
+            while i < x and not s[x].isalnum():
                 x -=1   
             if(s[i].lower() != s[x].lower()):
                 return False
-                
+            print(s[i].lower(), s[x].lower())    
             i += 1
             x -= 1
         return True
+        
 
 #       string = "".join([i for i in s if i.isalpha()])
 #       x = len(string) - 1
