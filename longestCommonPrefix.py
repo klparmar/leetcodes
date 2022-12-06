04c0dd0ef13b0c1015e590396105ea0cf0653a4d
KL Parmar <58920075+klparmar@users.noreply.github.com>
2022-12-06 01:27:15 -0500

    14. Longest Common Prefix
    Runtime: 56 ms, faster than 69.96% of Python3 online submissions for Longest Common Prefix.
    Memory Usage: 13.9 MB, less than 49.93% of Python3 online submissions for Longest Common Prefix.

---
+++ longestCommonPrefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = strs[0]
+        
+        for i in strs:
+            for j in range(len(prefix)):
+                if len(i) <= j:
+                    prefix = i
+                elif i[j] != prefix[j]:
+                    prefix = prefix[:j]
+                    break
+                    
+            if prefix == "":
+                return prefix
+        return prefix
