706bea174f794477a580116107e8f318e8dcd9b9
KL Parmar <theklparmar@gmail.com>
2023-08-04 20:10:19 -0400

    Create defangIPaddr.py
    1108. Defanging an IP Address

---
+++ defangIPaddr.py
@@ -0,0 +1,14 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace(".", "[.]")
+
+
+
+# Runtime
+# Details
+# 42ms
+# Beats 98.78%of users with JavaScript
+# Memory
+# Details
+# 41.52mb
+# Beats 77.35%of users with JavaScript
